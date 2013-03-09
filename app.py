2ff5c369b86a535b4cb994778f9096a95f946833
YuviPanda <yuvipanda@gmail.com>
2013-03-10 05:09:47 +0530

    Always listen on port 80 locally (helps with testing)
    Along with an /etc/hosts entry

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def show(filename):
     return render_template("shim.html", **data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True, port=80)
